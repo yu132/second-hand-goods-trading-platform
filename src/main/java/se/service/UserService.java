77a4c0b3cf3ae2b4fcfce1faab3e79e7307c8eea
18355010305 <1966865300@qq.com>
2018-11-01 18:00:58 +0800

    需考虑空值测试

--- src/main/java/se/service/UserService.java
+++ src/main/java/se/service/UserService.java
@@ -8,12 +8,16 @@ import org.springframework.stereotype.Service;
 
 import se.model.UserInfo;
 import se.repositories.UserInfoRepository;
+import se.service.judge.UserServiceJudge;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserInfoRepository userInfoRepository;
+	
+	@Autowired
+	private UserServiceJudge userServiceJudge;
 	
 	public Map<String,Object> login(String userName,String password){
 		Map<String,Object> result=new HashMap<>();
@@ -23,7 +27,16 @@ public class UserService {
 	
 	public Map<String,Object> register(UserInfo userInfo){
 		//TODO
-		return null;
+		Map<String,Object> result=new HashMap<>();
+		String userName=userInfo.getUserName();
+		if(userName.length()<6){
+			result.put("State", "");
+			return result;
+		}else if(userName.length()>20){
+			
+		}
+		userInfoRepository.save(userInfo);
+		return result;
 	}
 	
 }

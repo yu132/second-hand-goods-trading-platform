8e8d324774796e2d1bcd69457e7cc0ccc0088c13
18355010305 <1966865300@qq.com>
2018-11-03 21:08:24 +0800

    State和Reason改为枚举类型

--- src/main/java/se/service/judge/UserServiceJudge.java
+++ src/main/java/se/service/judge/UserServiceJudge.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import se.model.UserInfo;
 import se.repositories.UserInfoRepository;
+import se.stateEnum.State;
+import se.reasonEnum.Reason;
 
 @Service
 public class UserServiceJudge {
@@ -29,22 +31,22 @@ public class UserServiceJudge {
 		Map<String,Object> result=new HashMap<>();
 		UserInfo userInfo=userInfoRepository.findByUserName(userName);
 		if("".equals(userName)||userName==null){
-			result.put("State", "ERROR");
-			result.put("Reason", "USERNAME_IS_NULL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.USERNAME_IS_NULL);
 		}else if(userInfo!=null){
-			result.put("State", "ERROR");
-			result.put("Reason", "USERNAME_EXIST");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.USERNAME_EXIST);
 		}else if(userName.length()<USERNAME_MIN_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "USERNAME_TOO_SHORT");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.USERNAME_TOO_SHORT);
 		}else if(userName.length()>USERNAME_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "USERNAME_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.USERNAME_TOO_LONG);
 		}else if(!judgeUserNameChar(userName)){
-			result.put("State", "ERROR");
-			result.put("Reason", "USERNAME_ILLEAGAL_CHARACTER");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.USERNAME_ILLEAGAL_CHARACTER);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		
 		return result;
@@ -52,74 +54,74 @@ public class UserServiceJudge {
 	public Map<String,Object> judgePassword(String password){
 		Map<String,Object> result=new HashMap<>();
 		if("".equals(password)||password==null){
-			result.put("State", "ERROR");
-			result.put("Reason", "PASSWORD_IS_NULL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PASSWORD_IS_NULL);
 		}else if(password.length()<PASSWORD_MIN_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "PASSWORD_TOO_SHORT");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PASSWORD_TOO_SHORT);
 		}else if(password.length()>PASSWORD_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "PASSWORD_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PASSWORD_TOO_LONG);
 		}else if(!judgePasswordChar(password)){
-			result.put("State", "ERROR");
-			result.put("Reason", "PASSWORD_ILLEAGAL_CHARACTER");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PASSWORD_ILLEAGAL_CHARACTER);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		return result;
 	}
 	public Map<String,Object> judgeNickName(String nickName){
 		Map<String,Object> result=new HashMap<>();
 		if("".equals(nickName)||nickName==null){
-			result.put("State", "ERROR");
-			result.put("Reason", "NICKNAME_IS_NULL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.NICKNAME_IS_NULL);
 		}else if(nickName.length()<NICKNAME_MIN_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "NICKNAME_TOO_SHORT");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.NICKNAME_TOO_SHORT);
 		}else if(nickName.length()>NICKNAME_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "NICKNAME_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.NICKNAME_TOO_LONG);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		return result;
 	}
 	public Map<String,Object> judgeEmail(String email){
 		Map<String,Object> result=new HashMap<>();
 		if("".equals(email)||email==null){
-			result.put("State", "ERROR");
-			result.put("Reason", "EMAIL_IS_NULL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.EMAIL_IS_NULL);
 		}else if(!judgeEmailChar(email)){
-			result.put("State", "ERROR");
-			result.put("Reason", "EMAIL_ILLEGAL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.EMAIL_ILLEGAL);
 		}else if(email.length()>EMAIL_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "EMAIL_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.EMAIL_TOO_LONG);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		return result;
 	}
 	public Map<String,Object> judgePhoneNumber(String phoneNumber){
 		Map<String,Object> result=new HashMap<>();
 		if(phoneNumber.length()>PHONE_NUMBER_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "PHONE_NUMBER_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PHONE_NUMBER_TOO_LONG);
 		}else if(!judgePhoneNumberChar(phoneNumber)){
-			result.put("State", "ERROR");
-			result.put("Reason", "PHONE_NUMBER_ILLEGAL");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.PHONE_NUMBER_ILLEGAL);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		return result;
 	}
 	public Map<String,Object> judgeAddress(String address){
 		Map<String,Object> result=new HashMap<>();
 		if(address.length()>ADDERSS_MAX_LENGTH){
-			result.put("State", "ERROR");
-			result.put("Reason", "ADDERSS_TOO_LONG");
+			result.put("State", State.ERROR);
+			result.put("Reason", Reason.ADDERSS_TOO_LONG);
 		}else{
-			result.put("State", "SUCCESS");
+			result.put("State", State.SUCCESS);
 		}
 		return result;
 	}
@@ -128,8 +130,8 @@ public class UserServiceJudge {
 		boolean isMatch = Pattern.matches(pattern, userName);
 	    return isMatch;
 	}
-	private boolean judgePasswordChar(String password){
-		String pattern = "(?:[a-z]|[A-Z]|_|[0-9]|!|@|#|\\$|%|\\^|&|\\*|\\(|\\)|\\+| |~|)+";
+	private boolean judgePasswordChar(String password){//!@#$%^&*(),<.>:;?~`-'+|"/=[]{}和数字、大小写字母、下划线、空格
+		String pattern = "(?:[a-z]|[A-Z]|_|[0-9]|!|@|#|\\$|%|\\^|&|\\*|\\(|\\)| |,|<|.|>|:|;|\\?|~|`|-|'|\\+|\\||\"|/|=|[|]|\\{|\\})+";
 		boolean isMatch = Pattern.matches(pattern, password);
 	    return isMatch;
 	}

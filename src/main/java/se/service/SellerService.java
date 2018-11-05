e0c755bc2f7e6471b26d64f89f42a72335241e7c
18355010305 <1966865300@qq.com>
2018-11-05 20:05:05 +0800

    添加成员变量

--- src/main/java/se/service/SellerService.java
+++ src/main/java/se/service/SellerService.java
@@ -2,12 +2,24 @@ package se.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import se.model.Goods;
+import se.model.UserInfo;
+import se.repositories.GoodsRepository;
 
 @Service
 public class SellerService {
+	@Autowired
+	private SellerService sellerService;
+	
+	@Autowired
+	private GoodsRepository goodssRepository;
+	
+	private UserInfo user;
+	
+	private Goods goods;
 
 	public Map<String,Object> addGoods(Integer userId,Goods good){
 		//TODO

367e2bd6a625e1e0bf96f98d51930c11e194bae8
18355010305 <1966865300@qq.com>
2018-11-13 20:13:03 +0800

    添加repositories注释

--- src/main/java/se/repositories/GoodsRepository.java
+++ src/main/java/se/repositories/GoodsRepository.java
@@ -10,11 +10,49 @@ import se.model.Goods;
 
 @Repository
 public interface GoodsRepository extends CrudRepository<Goods, Integer>, JpaRepository<Goods, Integer> {
+	/**
+	 * 根据卖家id和物品名找物品
+	 * @param sellerId
+	 * @param goodsName
+	 * @return
+	 */
 	public Goods findBySellerIdAndGoodsName(Integer sellerId,String goodsName);
+	/**
+	 * 根据卖家id找物品，用于判断该卖家是否有物品
+	 * @param sellerId
+	 * @return
+	 */
 	public Goods findBySellerId(Integer sellerId);
+	/**
+	 * 返回该卖家商品数量
+	 * @param sellerId
+	 * @return
+	 */
 	public long countBySellerId(Integer sellerId);
+	/**
+	 * 返回某种状态的所有物品
+	 * @param state
+	 * @return
+	 */
 	public Iterable<Goods> findAllByState(String state);
+	/**
+	 * 返回某种状态的所有物品数量
+	 * @param state
+	 * @return
+	 */
 	public long countByState(String state);
+	/**
+	 * 分页返回某种状态的物品，时间降序
+	 * @param state
+	 * @param pageable
+	 * @return
+	 */
 	public Page<Goods> findByStateOrderByCommitTimeDesc(String state, Pageable pageable);
+	/**
+	 *  分页返回某卖家的物品，时间降序
+	 * @param sellerId
+	 * @param pageable
+	 * @return
+	 */
 	public Page<Goods> findBySellerIdOrderByCommitTimeDesc(Integer sellerId,Pageable pageable);
 }
--- src/main/java/se/repositories/UserInfoRepository.java
+++ src/main/java/se/repositories/UserInfoRepository.java
@@ -8,6 +8,17 @@ import se.model.UserInfo;
 
 @Repository
 public interface UserInfoRepository extends CrudRepository<UserInfo, Integer>, JpaRepository<UserInfo, Integer> {
+	/**
+	 * 用用户名和密码寻找一条用户信息
+	 * @param userName
+	 * @param password
+	 * @return
+	 */
 	public UserInfo findByUserNameAndPassword(String userName,String password);
+	/**
+	 * 用用户名寻找一条用户信息
+	 * @param userName
+	 * @return
+	 */
 	public UserInfo findByUserName(String userName);
 }
